4cce753c757d38e6fd0f5b06cb3da817370d3df7
Allene Norton <norton.allene@gmail.com>
2020-06-23 16:49:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,7 @@ def join_nested_strings(src)
     while el_index < src[row_index] do
       if src[row_index][el_index].is_a? == String
         nu_str = "" + [el_index]
+      else nil 
       end
     el_index += 1
     end
